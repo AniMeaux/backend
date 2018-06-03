4a5420bd4eb40f529236633d8b5f394e2fc09a12
WilliamDASILVA <william.da.silva@outlook.com>
2018-06-03 20:59:23 +0200

    Merge branch 'release/0.1.0'

--- db/migrate/20180527142600_create_animal_images.rb
+++
@@ -1,8 +0,0 @@
-class CreateAnimalImages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :animal_images do |t|
-      t.references :animal, foreign_key: true
-      t.references :image, foreign_key: true
-    end
-  end
-end
